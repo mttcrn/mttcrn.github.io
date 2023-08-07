b2f0b766020050469437483c49902fc8c6ebd7b9
Caterina Motti <motti.caterina@gmail.com>
2023-08-07 18:06:51 +0200

    Day/night switch
    Fixed javascript.

--- scripts.js
+++ scripts.js
@@ -1,53 +1,76 @@
-$(document).ready(function(){
-    $(".hamburger").on("click", function() {
-      $(".menu").toggleClass("menu_open");
-      if (document.querySelector(".menu").classList.contains("menu_open")) {
-          $(".btn").toggleClass('active');
-          $(".btn").removeClass('not-active');
-          document.body.style.overflow = 'hidden';
-      } else {
-        $(".btn").toggleClass('not-active');
-        $(".btn").removeClass('active');
-        setTimeout(function() {
-            document.body.style.overflow = 'visible';
-        }, 1000);
-      }
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.querySelector(".hamburger");
+    const menu = document.querySelector(".menu");
+    const body = document.body;
+    const switchInput = document.getElementById("switch");
+    const arrow = document.querySelector(".arrow");
+
+    //hamberger menu open/close
+    hamburger.addEventListener("click", function () {
+        menu.classList.toggle("menu_open");
+        if (menu.classList.contains("menu_open")) {
+            document.body.style.overflow = "hidden";
+            body.classList.add("active");
+            body.classList.remove("not-active");
+        } else {
+            body.classList.add("not-active");
+            body.classList.remove("active");
+            setTimeout(function () {
+                document.body.style.overflow = "visible";
+            }, 1000);
+        }
     });
 
-    $(".menu a").on("click", function(){
-        $(".menu").removeClass("menu_open");
-        $(".btn").toggleClass('not-active');
-        $(".btn").removeClass('active');
-        setTimeout(function() {
-            document.body.style.overflow = 'visible';
-        }, 1000);
+    //hamburger menu open/close when a section is selected
+    const menuLinks = document.querySelectorAll(".menu a");
+    menuLinks.forEach(function (link) {
+        link.addEventListener("click", function () {
+            menu.classList.remove("menu_open");
+            body.classList.add("not-active");
+            body.classList.remove("active");
+            setTimeout(function () {
+                document.body.style.overflow = "visible";
+            }, 1000);
+        });
     });
 
-    $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
-      if (scroll >= 1.5) {
-        $('.arrow').addClass('fade');
-      } else{
-        $('.arrow').removeClass('fade');
-      }
+    //arrow fade
+    window.addEventListener("scroll", function () {
+        if (window.scrollY >= 1.5) {
+            arrow.classList.add("fade");
+        } else {
+            arrow.classList.remove("fade");
+        }
     });
 
+    //day-night scroll
+    switchInput.addEventListener("click", function () {
+        if (this.checked) {
+            body.classList.add("day");
+        } else {
+            body.classList.remove("day");
+        }
+    });
+
+    //ScrollReveal initialization
+    ScrollReveal().reveal(".reveal", {
+        distance: "100px",
+        duration: 1500,
+        easing: "cubic-bezier(.215, .61, .355, 1)",
+        interval: 600,
+    });
+
+    //intro text animation
+    const typeElement = document.getElementById("type");
+    const introString = "Hi! I'm Cate";
+    let charIndex = 0;
+
+    function typeWriter() {
+        if (charIndex < introString.length) {
+            typeElement.innerHTML += introString.charAt(charIndex);
+            charIndex++;
+            setTimeout(typeWriter, 100);
+        }
+    }
+    typeWriter();
 });
-
-ScrollReveal().reveal('.reveal', {
-  distance: '100px', duration: 1500, easing: 'cubic-bezier(.215, .61, .355, 1)', interval: 600
-});
-
-var string = "Hi! I'm Cate";
-var array = string.split("");
-var timer;
-
-function frameLooper () {
-  if (array.length > 0) {
-    document.getElementById("type").innerHTML += array.shift();
-  } else {
-    clearTimeout(timer);
-  }
-  loopTimer = setTimeout('frameLooper()', 100); /* change number for speed */
-}
-frameLooper();
